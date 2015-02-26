339af25d2a6f6e95146398a0e6b0057f8b1eecf2
William Bruno <rocha_bruno@hotmail.com>
2015-02-26 13:43:29 -0300

    Garderning upload

--- upload-ajax/upload.php
+++ upload-ajax/upload.php
@@ -1,12 +1,14 @@
 <?php
 
-header('Content-Type: application/json');
-if (isset($_FILES['file'])) {
+if (isset($_FILES['file']) && !empty($_FILES['file']['name'])) {
   file_put_contents('uploads/post.txt', 'name=' . $_POST['name'] . ',count=' . $_POST['i'] . PHP_EOL, FILE_APPEND);
   move_uploaded_file($_FILES['file']['tmp_name'], "uploads/" . $_FILES['file']['name']);
 
-  echo json_encode('{ status: "ok" }');
+  $ret = array('status' => 'ok');
 } else {
-  echo json_encode('{ error: "no_file" }');
+  $ret = array('error' => 'no_file');
 }
+
+header('Content-Type: application/json');
+echo json_encode($ret);
 exit;
